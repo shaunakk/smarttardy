b279ca83827a3cca8ee20fc8da61399fb8b48bee
BASEHacks <BASEHacks@e1z3r9p15.42.us.org>
2017-04-09 01:40:08 -0700

    hi

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-hello
+$(document).ready(
+  function(){
+    $("#leway").on("input",
+      function(){
+        $("#lewaytime").html($("#leway").attr("value"))
+        console.log("hi")
+      }
+
+    )
+  }
+)
